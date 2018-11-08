80cc434a4a3e2d5b57002dad7793e625eac9907d
houarizegai <houarizegai14@gmail.com>
2018-11-08 19:17:57 +0100

    Adding ComputePrefix in KMP Algorithm

--- src/com/houarizegai/string/kmp/KMP.java
+++ src/com/houarizegai/string/kmp/KMP.java
@@ -5,10 +5,41 @@ package com.houarizegai.string.kmp;
 */
 public class KMP {
 
-    private int[] computePrefixArray(char[] pattern) {
-        int[] prefixArray = new int[pattern.length];
+    public int[] computePrefixArray(char[] pattern) {
+        int[] result = new int[pattern.length];
+        result[0] = 0;
+        for(int i = 1; i < result.length; i++)
+            result[i] = -1;
+        int i = 1;
+        int j = 0;
 
-        return prefixArray;
+        while(i < pattern.length) {
+            if(pattern[i] == pattern[j]) {
+                result[i] = j + 1;
+                i++;
+                j++;
+            } else {
+                if(j == 0) {
+                    result[i++] = 0;
+                } else {
+                    j = result[j - 1];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public void printArray(int[] array) { // print array of integer
+        for(int item : array)
+            System.out.print(item + " ");
+        System.out.println();
+    }
+
+    public void printArray(char[] array) { // print array of integer
+        for(char item : array)
+            System.out.print(item + " ");
+        System.out.println();
     }
 
 }
--- src/com/houarizegai/string/kmp/KMPDemo.java
+++ src/com/houarizegai/string/kmp/KMPDemo.java
@@ -2,9 +2,12 @@ package com.houarizegai.string.kmp;
 
 public class KMPDemo {
     public static void main(String[] args) {
-        String text = "";
+        String text = "aabaabaaa";
         String pattern = "";
-
-
+        KMP kmp = new  KMP();
+        System.out.print("Input:\t");
+        kmp.printArray(text.toCharArray());
+        System.out.print("Output:\t");
+        kmp.printArray(kmp.computePrefixArray(text.toCharArray()));
     }
 }
