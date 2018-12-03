5e177c6b1f241dadd51216e5642becbc034f2128
HouariZEGAI <houarizegai14@gmail.com>
2018-12-03 19:21:04 +0100

    Adding Egyptian Multiplication

---
+++ src/com/houarizegai/math/egyptian_math/EgyptianMultiplicationDemo.java
@@ -0,0 +1,25 @@
+package com.houarizegai.math.egyptian_math;
+
+public class EgyptianMultiplicationDemo {
+
+    private static int getMul(int a, int b) { // Calc multiplication (Get A * B)
+        int result = 0;
+        while(b >= 1) {
+            if(b % 2 != 0)
+                result += a;
+
+            b /= 2;
+            a *= 2;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int a = 125;
+        int b = 91;
+        int result = EgyptianMultiplicationDemo.getMul(a, b);
+
+        System.out.println(a + " * " + b + " = " + result);
+    }
+}
