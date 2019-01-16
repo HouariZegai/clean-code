791b674aad30f694f0082ea21db2436628f43cca
HouariZEGAI <houarizegai14@gmail.com>
2019-01-16 09:38:07 +0100

    Adding DFS algorithm

---
+++ src/com/houarizegai/dfs/AdjList.java
@@ -0,0 +1,13 @@
+package com.houarizegai.dfs;
+
+public class AdjList {
+    private Node head;
+
+    public Node getHead() {
+        return head;
+    }
+
+    public void setHead(Node head) {
+        this.head = head;
+    }
+}
