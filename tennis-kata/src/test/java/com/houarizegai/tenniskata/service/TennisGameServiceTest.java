cf7e2f412d01a77c1b717160dd010bc1e58f39cf
houarizegai <houarizegai14@gmail.com>
2021-12-05 21:55:13 +0100

    wrote first failing test

---
+++ tennis-kata/src/test/java/com/houarizegai/tenniskata/service/TennisGameServiceTest.java
@@ -0,0 +1,12 @@
+package com.houarizegai.tenniskata.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class TennisGameServiceTest {
+
+    public void shouldReturnLoveAll() {
+        TennisgameService tennisGameService = new TennisGameService();
+    }
+}
