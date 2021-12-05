0476276c8c1621bd9a54f0c398d81821e0ba0c27
houarizegai <houarizegai14@gmail.com>
2021-12-05 22:58:11 +0100

    refactor

--- tennis-kata/src/main/java/com/houarizegai/tenniskata/service/impl/TennisGameServiceImpl.java
+++ tennis-kata/src/main/java/com/houarizegai/tenniskata/service/impl/TennisGameServiceImpl.java
@@ -3,24 +3,28 @@ package com.houarizegai.tenniskata.service.impl;
 import com.houarizegai.tenniskata.service.TennisGameService;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class TennisGameServiceImpl implements TennisGameService {
 
     private int firstPlayerScore;
+    private static final Map<Integer, String> SCORES = new HashMap<>();
+
+    static {
+        SCORES.put(1, "Fifteen");
+        SCORES.put(2, "Thirty");
+        SCORES.put(3, "Forty");
+    }
 
     @Override
     public String getScore() {
-        if(firstPlayerScore == 1) {
-            return "Fifteen Love";
-        }
-        if(firstPlayerScore == 2) {
-            return "Thirty Love";
-        }
-        if(firstPlayerScore == 3) {
-            return "Forty Love";
+        if(firstPlayerScore == 0) {
+            return "Love All";
         }
 
-        return "Love All";
+        return SCORES.get(firstPlayerScore) + " Love";
     }
 
     @Override
