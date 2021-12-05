c15fab740bbf0cd0715b0d749dd64380464e6156
houarizegai <houarizegai14@gmail.com>
2021-12-05 22:02:24 +0100

    failing test

--- tennis-kata/src/test/java/com/houarizegai/tenniskata/service/TennisGameServiceTest.java
+++ tennis-kata/src/test/java/com/houarizegai/tenniskata/service/TennisGameServiceTest.java
@@ -1,13 +1,18 @@
 package com.houarizegai.tenniskata.service;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 public class TennisGameServiceTest {
 
+    @Test
     public void shouldReturnLoveAll() {
         TennisGameService tennisGameService = new TennisGameService();
         String score = tennisGameService.getScore();
+        assertEquals("Love All", score);
     }
 }
